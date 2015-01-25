f08b847710cd50fd85d5a0936465bdd7911cf619
Nathen Watters <wattersnathen@gmail.com>
2015-01-24 19:18:23 -0700

    adjusted code formatting

--- src/ShoppingList/ShoppingListPage.java
+++ src/ShoppingList/ShoppingListPage.java
@@ -11,48 +11,48 @@ import org.openqa.selenium.support.PageFactory;
 
 public class ShoppingListPage {
 	
-	// -----------------------------------
-	// PageFactory WebElements, use @CacheLookup for static WebElements
-	// ----------------------------------- 
-	@FindBy(id="entered-item-text")
-	@CacheLookup
-	private WebElement enterItem;
-	
-	@FindBy(id="entered-item-quantity")
-	@CacheLookup
-	private WebElement enterQuantity;
-	
-	@FindBy(id="add-button")
-	@CacheLookup
-	private WebElement addItem;
-	
-	@FindBy(id="clear-options")
-	@CacheLookup
-	private WebElement clearLists;
-	
-	@FindBy(id="save-options")
-	@CacheLookup
-	private WebElement saveLists;
-	
-	@FindBy(id="items-to-purchase")
-	private WebElement itemsList;
-	
-	@FindBy(id="items-purchased")
-	private WebElement purchasedList;
-	 
-	private String url = "http://wattersnathen.github.io/shopping-list-app/";
-	private WebDriver driver;
-	
-	/**
-	 * ShoppingListPage constructor. Calls initElements
-	 * from the PageFactory class.
-	 * @param driver
-	 */
-	public ShoppingListPage(WebDriver driver) {
-		this.driver = driver;
-		PageFactory.initElements(this.driver, this);
-	}
-	
+    // -----------------------------------
+    // PageFactory WebElements, use @CacheLookup for static WebElements
+    // ----------------------------------- 
+    @FindBy(id="entered-item-text")
+    @CacheLookup
+    private WebElement enterItem;
+    
+    @FindBy(id="entered-item-quantity")
+    @CacheLookup
+    private WebElement enterQuantity;
+    
+    @FindBy(id="add-button")
+    @CacheLookup
+    private WebElement addItem;
+    
+    @FindBy(id="clear-options")
+    @CacheLookup
+    private WebElement clearLists;
+    
+    @FindBy(id="save-options")
+    @CacheLookup
+    private WebElement saveLists;
+    
+    @FindBy(id="items-to-purchase")
+    private WebElement itemsList;
+    
+    @FindBy(id="items-purchased")
+    private WebElement purchasedList;
+     
+    private String url = "http://wattersnathen.github.io/shopping-list-app/";
+    private WebDriver driver;
+    
+    /**
+     * ShoppingListPage constructor. Calls initElements
+     * from the PageFactory class.
+     * @param driver
+     */
+    public ShoppingListPage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(this.driver, this);
+    }
+    
     /**
      * Public access to all of the items on the current page.
      * @return list of WebElements found on the page when function was called
@@ -60,7 +60,7 @@ public class ShoppingListPage {
     public List<WebElement> getAllItemsOnPage() {
         return this.driver.findElements(By.className("item"));
     }
-    
+        
     /**
      * Get a reference to an item on the page if it exists.
      * @param itemToFind text value of the item to find 
@@ -73,51 +73,51 @@ public class ShoppingListPage {
                 return item;
             }
         }
-        
+    
         return null; // Item was not found on the page
     }
-	
-	/**
-	 * Add an item to the items list.
-	 * @param itemToAdd 
-	 * @param itemQuantity
-	 */
-	public void addItemToList(String itemToAdd, int itemQuantity) {
-		this.enterItem.sendKeys(itemToAdd);
-		// the enterQuantity input has been defaulted to a value of 1, clear it first
-		this.enterQuantity.clear();
-		
-		// itemQuantity field must be 1 or greater. Default to 1 if value less than 1 is issued.
-		if (itemQuantity < 1) {
-		    this.enterQuantity.sendKeys("1");
-		}
-		this.enterQuantity.sendKeys(Integer.toString(itemQuantity));
-		this.addItem.click();
-	}
-	
-	/**
-	 * Delete item from the shopping list application.
-	 * @param itemToDelete
-	 */
-	public void deleteItemFromPage(String itemToDelete) {
-	    WebElement deletableItem = findItemOnPage(itemToDelete);
-	    if (deletableItem != null) {
-	        deletableItem.findElement(By.cssSelector(".btn-delete")).click();
-	    }
-	    // TODO: implement handler for when the item isn't found on the page.
-	    else {
-	        
-	    }
-	}
-	
-	/**
-	 * Click the 'Clear List(s)' button to remove all items from the page.
-	 * NOTE: Also destroys the localStorage API data -- which is a feature of the site itself.
-	 */
-	public void deleteAllItemsFromThePage() {
-	    this.clearLists.click();
-	}
-	
+    
+    /**
+     * Add an item to the items list.
+     * @param itemToAdd 
+     * @param itemQuantity
+     */
+    public void addItemToList(String itemToAdd, int itemQuantity) {
+        this.enterItem.sendKeys(itemToAdd);
+        // the enterQuantity input has been defaulted to a value of 1, clear it first
+        this.enterQuantity.clear();
+    
+        // itemQuantity field must be 1 or greater. Default to 1 if value less than 1 is issued.
+        if (itemQuantity < 1) {
+            this.enterQuantity.sendKeys("1");
+        }
+        this.enterQuantity.sendKeys(Integer.toString(itemQuantity));
+        this.addItem.click();
+    }
+    
+    /**
+     * Delete item from the shopping list application.
+     * @param itemToDelete
+     */
+    public void deleteItemFromPage(String itemToDelete) {
+        WebElement deletableItem = findItemOnPage(itemToDelete);
+        if (deletableItem != null) {
+            deletableItem.findElement(By.cssSelector(".btn-delete")).click();
+        }
+        // TODO: implement handler for when the item isn't found on the page.
+        else {
+            
+        }
+    }
+    
+    /**
+     * Click the 'Clear List(s)' button to remove all items from the page.
+     * NOTE: Also destroys the localStorage API data -- which is a feature of the site itself.
+     */
+    public void deleteAllItemsFromThePage() {
+        this.clearLists.click();
+    }
+    
     /**
      * Load this page. Calls the WebDriver.get(URL) method.
      */
